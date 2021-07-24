c25978ea63928eaec29008a92ee2fa3d6f2771af
kkurbatskyi <57308179+kkurbatskyi@users.noreply.github.com>
2021-07-23 23:18:16 -0700

    Cleaning up the code: resolving warnings

--- src/main/kotlin/Circle.kt
+++ src/main/kotlin/Circle.kt
@@ -1,18 +1,11 @@
 import java.awt.Color
 
 class Circle(_x: Int, _y: Int, _diameter: Float) {
-    val colors: List<Color> = listOf(Color.GREEN, Color.MAGENTA, Color.RED, Color.BLUE, Color.ORANGE, Color.PINK, Color.YELLOW, Color.CYAN)
-    val color: Color
-    var x: Int
-    var y: Int
-    var diameter: Float
-
-    init{
-        x = _x
-        y = _y
-        diameter = _diameter
-        color = colors.random()
-    }
+    private val colors: List<Color> = listOf(Color.GREEN, Color.MAGENTA, Color.RED, Color.BLUE, Color.ORANGE, Color.PINK, Color.YELLOW, Color.CYAN)
+    val color: Color = colors.random()
+    var x: Int = _x
+    var y: Int = _y
+    var diameter: Float = _diameter
 
     fun radius(): Float{
         return diameter / 2
